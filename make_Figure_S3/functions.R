0f889e7100bfd225fbab6282977e9561da63af8c
SiminaB <simina@poisson>
2016-08-07 00:11:53 -0400

    Changed some colors, linetypes in Figure S3

--- make_Figure_S3/functions.R
+++ make_Figure_S3/functions.R
@@ -165,3 +165,9 @@ invert2By2Var <- function(v)
   dInv
 }
 
+##get default ggplot2 colors
+##from http://stackoverflow.com/questions/8197559/emulate-ggplot2-default-color-palette
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
